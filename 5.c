0ce6789ee7941a66f479c327ae0f31ca734e3220
Breit1 <108278368+Breit1@users.noreply.github.com>
2023-05-05 23:47:36 +0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	int x;
+	int x, cg = 0, a, b = 0;
+    double sredar;
     scanf("%d", &x);
     while (x!=0){
     scanf("%d", &x);
-    
+    a = x;
+    b = b + a;
+    cg = cg + 1;
     }
+    sredar = (double)b / (double)(cg-1);
+    printf("%f", sredar);
 
 }
